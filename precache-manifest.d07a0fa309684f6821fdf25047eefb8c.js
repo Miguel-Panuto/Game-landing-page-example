8461634b7a66fc1b5a886a58e2eb127709204993
Miguel Panuto <miguel.panuto@live.com>
2020-04-20 18:11:23 -0300

    Updates

---
+++ precache-manifest.d07a0fa309684f6821fdf25047eefb8c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623a7a55ed095f0be9bd166ea7e6afca",
+    "url": "/game-landing-page-example/index.html"
+  },
+  {
+    "revision": "8425dfa83d7d4e72a5e3",
+    "url": "/game-landing-page-example/static/js/2.8056dc62.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/game-landing-page-example/static/js/2.8056dc62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89f25a395ee1f821e49f",
+    "url": "/game-landing-page-example/static/js/main.574d2b8f.chunk.js"
+  },
+  {
+    "revision": "fbd7b4a387399babdf48",
+    "url": "/game-landing-page-example/static/js/runtime-main.7c12502e.js"
+  },
+  {
+    "revision": "03311ef719c17bd35912e8d4b916f175",
+    "url": "/game-landing-page-example/static/media/fossil_cave.03311ef7.jpg"
+  },
+  {
+    "revision": "633d7bef165476e96d8d5680b0fd2dfc",
+    "url": "/game-landing-page-example/static/media/game-controller.633d7bef.svg"
+  },
+  {
+    "revision": "254d452cc120266863130f4bd4b72725",
+    "url": "/game-landing-page-example/static/media/star.254d452c.svg"
+  },
+  {
+    "revision": "a589d6702625773f61f2d15002bc6dc1",
+    "url": "/game-landing-page-example/static/media/users.a589d670.svg"
+  }
+]);
